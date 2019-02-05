a6163d92b6a0e609907f92fb5d13b7ac6ddb8329
legkvla <vladimir.legkunets@gmail.com>
2019-02-05 10:20:34 +0300

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to helda-owgame-cli
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/helda_owgame_cli/core_test.clj
@@ -0,0 +1,7 @@
+(ns helda-owgame-cli.core-test
+  (:require [clojure.test :refer :all]
+            [helda-owgame-cli.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
