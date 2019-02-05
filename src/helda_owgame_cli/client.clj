2482bfdeee4c878f87c8ada16eb936f12c489131
legkvla <vladimir.legkunets@gmail.com>
2019-02-05 10:41:16 +0300

    Design for game client added

---
+++ src/helda_owgame_cli/client.clj
@@ -0,0 +1,14 @@
+(ns helda-owgame-cli.client
+  (:require
+    [clj-http.client :as client]
+    )
+  )
+
+(defn find-entities
+  ([models])
+  ([models tags])
+  )
+
+(defn lookup-entity [id]
+
+  )
--- src/helda_owgame_cli/core.clj
+++ src/helda_owgame_cli/core.clj
@@ -1,6 +1,7 @@
 (ns helda-owgame-cli.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn new-game [])
+
+(defn encounter [])
+
+(defn fight [])
